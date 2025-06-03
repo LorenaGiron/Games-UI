b4c00da92cc04046a3d6632b94d6c8862185026d
Mr.T <terryrangel04@gmail.com>
2025-06-02 22:29:09 -0600

    Cambio de Diseño Categorias -mejora

--- js/categorias.js
+++ js/categorias.js
@@ -60,7 +60,7 @@ row.className = 'row g-4';
 
 gameDetails.tags.forEach((genre, index) => {
   const col = document.createElement('div');
-  col.className = 'col-12 col-sm-6 col-md-4 col-lg-3';
+  col.className = 'col-11 col-sm-6 col-md-4 col-lg-3';
 
   const card = document.createElement('div');
   card.className = 'card genre-card h-100 text-center';
